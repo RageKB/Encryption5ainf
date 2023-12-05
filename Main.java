f1b2603d64df26a6fcdce6cc14612a475a4e32e4
RageKB <ragekb1@gmail.com>
2023-12-05 09:38:08 +0000

    ciao

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Cifriamo il nostro testo e trasformiamolo in un testo segreto!");
+
+    matrice m = new matrice(TPSIT);
+    //istanziare un oggetto di tipo Vigenere
+    Vigenere v1 = new Vigenere(0,12,0,12,m)
+
+      Thread t =  new Thread(p)
+      t1.start();
+    //ciao
+  }
+}
+//istanziare un thread
